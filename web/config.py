93e077cb560f87707c1acdb368f6f4a6288c6cb4
mad4a <chsc4698@gmail.com>
2014-06-25 11:33:58 +0800

    remove web/config.py

--- web/config.py
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-import logging
-from stream import ImageStream, WindowsPhysicalDriveStream
-
-stream = ImageStream('d:/flash.raw')
-address, port = '127.0.0.1', 8000
-
-partition_log_level = logging.INFO
