5e9e2cdd1dbd6a135dcbea18d4b7cbb5d66811d9
Hang Chen <chsc4698@gmail.com>
2014-07-12 14:26:28 +0800

    add supported partition types dict

--- drive/boot_sector/_boot_sector.py
+++ drive/boot_sector/_boot_sector.py
@@ -6,6 +6,7 @@
     This module implements the meta struct of the various boot sectors.
 """
 from construct import *
+from drive.boot_sector.misc import supported_partition_types
 from ..keys import *
 from misc import MAGIC_END_SECTION
 
@@ -51,14 +52,10 @@ def _partition_entry_template(abs_pos):
               calc_chs_address(k_starting_chs_address)),
 
         Byte(k_partition_type),
-        Value(k_partition_type, lambda c: {
-            0x0: k_ignored,
-            0x5: k_ExtendedPartition,
-            0xf: k_ExtendedPartition,
-            0xb: k_FAT32,
-            0xc: k_FAT32,
-            0x7: k_NTFS,
-        }[c[k_partition_type]]),
+        Value(k_partition_type, lambda c: supported_partition_types.get(
+            c[k_partition_type],
+            str(c[k_partition_type]
+        ))),
 
         Array(3, ULInt8(k_ending_chs_address)),
         Value(k_ending_chs_address,
