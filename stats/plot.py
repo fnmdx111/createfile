3921b7c8e264678491e4d353da9e60b6b968668c
mad4a <chsc4698@gmail.com>
2014-07-12 13:29:18 +0800

    vectorize plot options

--- stats/plot.py
+++ stats/plot.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 def plot_windowed_metrics(normal_data, abnormal_data,
                           line_data, fn, dot_formats,
-                          plot_normal=True,
-                          plot_abnormal=True,
+                          plot_normal=None,
+                          plot_abnormal=None,
                           figure=None, subplot_n=111,
                           show=False):
     """Plot metrics values. You may want to clean your
@@ -29,22 +29,27 @@ def plot_windowed_metrics(normal_data, abnormal_data,
     figure = figure or plt.figure()
     ax = figure.add_subplot(subplot_n)
 
+    plot_normal = plot_normal or [True for _ in fn]
+    plot_abnormal = plot_abnormal or [True for _ in plot_normal]
+
     plots, names = [], []
-    for n, vs, fmt in zip(fn,
-                          zip(line_data, normal_data, abnormal_data),
-                          dot_formats):
+    for n, vs, fmt, pn, pa in zip(fn,
+                                  zip(line_data, normal_data, abnormal_data),
+                                  dot_formats,
+                                  plot_normal,
+                                  plot_abnormal):
 
         # plot background line
         names.append(n)
         plots.append(ax.plot(vs[0][0], vs[0][1], fmt[2:],
                              label=names[-1])[0])
-        if plot_normal:
+        if pn:
             # plot normal dots
             names.append('%s - %s' % (n, 'normal'))
 
             plots.append(ax.plot(vs[1][0], vs[1][1], 'g%s' % fmt[0],
                                  label=names[-1])[0])
-        if plot_abnormal:
+        if pa:
             # plot abnormal dots
             names.append('%s - %s' % (n, 'abnormal'))
 
