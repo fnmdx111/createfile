1fec74637731d7601bd0929e23fdbc5d81c44659
mad4a <chsc4698@gmail.com>
2014-07-28 19:35:51 +0800

    add conclusions for abnormal rules of ntfs

--- judge/built_in/ntfs.py
+++ judge/built_in/ntfs.py
@@ -28,31 +28,31 @@ rule5 = ['approx_eq(SI_M, SI_E, SI_A)'
 rule6 = ['approx_eq(SI_E, SI_A)'
       ' & (min_(SI_E, SI_A) > max_(SI_C, FN_ALL))'
       ' & (_.SI_E != _.SI_M)',
-         '未命名异常',
+         '编辑文件的M被修改',
          True]
 
 rule7 = ['(_.SI_E > max_(FN_ALL, SI_M))'
       ' & approx_eq(FN_ALL, SI_M)'
       ' & (min_(FN_ALL, SI_M) > _.SI_C)',
-         '未命名异常',
+         '重命名文件的C往前改',
          True]
 
 rule8 = ['(_.SI_E > max_(SI_A, FN_ALL))'
       ' & approx_eq(SI_A, FN_ALL)'
       ' & (min_(SI_A, FN_ALL) > _.SI_M)'
       ' & (_.SI_C != _.SI_E)',
-         '未命名异常',
+         '复制的文件修改C',
          True]
 
 rule9 = ['(_.SI_E > max_(SI_C, FN_ALL))'
       ' & approx_eq(SI_C, FN_ALL)'
       ' & (min_(SI_C, FN_ALL) > _.SI_M)'
       ' & (_.SI_A != _.SI_E)',
-          '未命名异常',
+          '复制的文件修改A',
           True]
 
 rule10 = ['(_.SI_M != _.SI_E)'
        ' & (min_(SI_M, SI_E) > max_(SI_C, SI_A, FN_ALL))'
        ' & approx_eq(SI_C, SI_A, FN_ALL)',
-          '未命名异常',
+          '复制的文件M往后改',
           True]
