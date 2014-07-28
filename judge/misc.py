d337b1d156cdec718a5df3a1a216cbeaeb12cbb9
mad4a <chsc4698@gmail.com>
2014-07-28 18:31:35 +0800

    enhance expressibility of the built-in DSL

--- judge/misc.py
+++ judge/misc.py
@@ -4,16 +4,12 @@ from itertools import combinations
 id_ = lambda _: _
 
 wrapper_func = lambda op, od: op(od)
-ex_wrapper_func = lambda op, od: map(op, od)
+
+def getattr_(x, n):
+    if n == '':
+        return x
+    else:
+        return getattr(x, n)
 
 predicate_od_func = lambda self, x: self.predicate(x)
-attribute_od_func = lambda self, x: getattr(x, self.name)
-
-def combinations_ex(xs, p1, p2):
-    xs = list(xs)
-
-    cs = combinations(range(len(xs)), 2)
-    p1s, p2s = list(p1(xs)), list(p2(xs))
-
-    for i, j in cs:
-        yield p1s[i], p2s[j]
+attribute_od_func = lambda self, x: getattr_(x, self.name)
--- judge/wrappers.py
+++ judge/wrappers.py
@@ -1,15 +1,8 @@
 # encoding: utf-8
-from functools import partial
-from itertools import repeat, product
 import operator
-from .misc import id_, wrapper_func, ex_wrapper_func, predicate_od_func, \
-    attribute_od_func, combinations_ex
+from .misc import id_, wrapper_func, predicate_od_func, \
+    attribute_od_func, getattr_
 
-def getattr_(x, n):
-    if n == '':
-        return x
-    else:
-        return getattr(x, n)
 
 class AbstractWrapper:
     def __init__(self, expr):
@@ -19,6 +12,8 @@ class AbstractWrapper:
             self.install_binary('__%s__' % kw, getattr_(operator, kw))
         self.install_binary('__and__', operator.and_)
         self.install_binary('__or__', operator.or_)
+        self.install_binary('__max__', max)
+        self.install_binary('__min__', min)
 
     def install_binary(self, name, op):
         dummy = self.gen_dummy(op)
@@ -75,19 +70,8 @@ class PredicateWrapper(AbstractWrapper):
                 else:
                     return op_(o1, o2)
 
-            if isinstance(other, PredicateWrapperEx):
-
-                raise NotImplementedError
-                # return PredicateWrapperEx(
-                #     # avoid comparing between PW and PWE objects
-                #     # all or any is not decidable
-                #     lambda xs: patch(op,
-                #                      all(map(self.predicate, xs)),
-                #                      other.predicate(xs)),
-                #     '%s(%s, %s)' % (op.__name__, self_.expr, other.expr)
-                # )
-            elif isinstance(other, AttributeWrapper):
-                other_ = lambda x: attribute_od_func(other, x)
+            if isinstance(other, AttributeWrapper):
+                other_ = lambda x: attribute_od_func(other, other.obj(x))
                 other_expr = other.expr
             elif type(other) != type(self_):
                 other_ = lambda _: other
@@ -110,16 +94,6 @@ class PredicateWrapper(AbstractWrapper):
     def __call__(self, *args, **kwargs):
         return self.predicate(*args, **kwargs)
 
-    def __all__(self):
-        return PredicateWrapperEx(lambda xs: [all(map(self.predicate,
-                                                      xs))],
-                                  'all(%s)' % self.expr)
-
-    def __any__(self):
-        return PredicateWrapperEx(lambda xs: [any(map(self.predicate,
-                                                      xs))],
-                                  'any(%s)' % self.expr)
-
 install_misc(PredicateWrapper,
              PredicateWrapper,
              wrapper_func,
@@ -136,10 +110,15 @@ class AttributeWrapper(AbstractWrapper):
 
         self.expr = expr or '%s.%s' % (self.parent.name, self.name)
 
-        self.non_attr=non_attr
+        self.non_attr = non_attr
 
     def gen_dummy(self, op):
         def dummy(self_, other):
+            if self.non_attr:
+                n = lambda x: self_.obj(x)
+            else:
+                n = lambda x: getattr_(self_.obj(x), self_.name)
+
             if type(other) == type(self_):
                 if other.non_attr:
                     other_ = lambda x: other.obj(x)
@@ -152,14 +131,12 @@ class AttributeWrapper(AbstractWrapper):
             elif isinstance(other, PredicateWrapper):
                 other_ = other.predicate
                 other_expr = other.expr
+            elif isinstance(other, Probe):
+                other_ = lambda x: print(n(x))
+                other_expr = ''
             else:
                 other_ = lambda _: other
                 other_expr = other
-
-            if self.non_attr:
-                n = lambda x: self_.obj(x)
-            else:
-                n = lambda x: getattr_(self_.obj(x), self_.name)
 
             return PredicateWrapper(lambda x: op(n(x),
                                                  other_(x)),
@@ -179,105 +156,11 @@ class AttributeWrapper(AbstractWrapper):
     def __getattr__(self, item):
         return AttributeWrapper(item, self, obj=lambda x: getattr_(self.obj(x),
                                                                   self.name))
-
 install_misc(AttributeWrapper,
              PredicateWrapper,
              wrapper_func,
              attribute_od_func)
 
 
-class PredicateWrapperEx(AbstractWrapper):
-    def __init__(self, predicate=id_, expr=''):
-        super(PredicateWrapperEx, self).__init__(expr)
-
-        self.predicate = predicate
-
-    def gen_dummy(self, op):
-        def dummy(self_, other):
-            if type(other) == type(self_):
-                return PredicateWrapperEx(
-                    lambda xs: map(lambda _: op(_[0], _[1]),
-                                   product(self_.predicate(xs),
-                                           other.predicate(xs))),
-                    '%s(%s, %s)' % (op.__name__,
-                                    self_.expr,
-                                    other.expr)
-                )
-            else:
-                return PredicateWrapperEx(
-                    lambda xs: map(lambda _: op(*_),
-                                   zip(self_.predicate(xs),
-                                       repeat(other))),
-                    '%s(%s, %s)' % (op.__name__,
-                                    self_.expr,
-                                    other)
-                )
-
-        return dummy
-
-    def __call__(self, *args, **kwargs):
-        return self.predicate(*args, **kwargs)
-
-    def __all__(self):
-        return PredicateWrapperEx(lambda xs: [all(self.predicate(xs))],
-                                  'all(%s)' % self.expr)
-
-    def __any__(self):
-        return PredicateWrapperEx(lambda xs: [any(self.predicate(xs))],
-                                  'any(%s)' % self.expr)
-
-install_misc(PredicateWrapperEx,
-             PredicateWrapperEx,
-             ex_wrapper_func,
-             predicate_od_func)
-
-
-class AttributeWrapperEx(AbstractWrapper):
-    def __init__(self, name, parent, expr=''):
-        super(AttributeWrapperEx, self).__init__(expr)
-
-        self.name = name
-        self.parent = parent
-
-        self.expr = expr or '%s.%s' % (self.parent.name, self.name)
-
-    def gen_dummy(self, op):
-        def dummy(self_, other):
-            other_expr = other
-            if type(other) == type(self_):
-                other_expr = other.expr
-                coll = lambda xs: combinations_ex(
-                    xs,
-                    partial(map,
-                            lambda x: getattr_(x, self_.name)),
-                    partial(map,
-                            lambda x: getattr_(x, other.name))
-                )
-            elif isinstance(other, PredicateWrapperEx):
-                other_expr = other.expr
-                coll = lambda xs: zip(map(lambda x: getattr_(x, self_.name),
-                                          xs),
-                                      other.predicate(xs))
-            else:
-                coll = lambda xs: zip(map(lambda x: getattr_(x, self_.name),
-                                          xs),
-                                      repeat(other))
-
-            return PredicateWrapperEx(
-                lambda xs: map(lambda _: op(*_),
-                               coll(xs)),
-                '%s(%s, %s)' % (op.__name__, self_.expr, other_expr)
-            )
-
-        return dummy
-
-install_misc(AttributeWrapperEx,
-             PredicateWrapperEx,
-             ex_wrapper_func,
-             attribute_od_func)
-
-all_ = lambda w: w.__all__()
-every = all_
-
-any_ = lambda w: w.__any__()
-some = any_
+class Probe:
+    pass
